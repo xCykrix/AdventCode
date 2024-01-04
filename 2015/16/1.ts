ec9fca99d9b95ea8c1022892ae93d595448c3650
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2024-01-03 22:58:58 -0500

    feat: 2015p16

--- 2015/16/1.ts
+++ 2015/16/1.ts
@@ -1,12 +1,60 @@
 import { AOC, InputType } from '../../util/state.ts';
 
+interface AuntSue {
+  children: number | null;
+  cats: number | null;
+  samoyeds: number | null;
+  pomeranians: number | null;
+  akitas: number | null;
+  vizslas: number | null;
+  goldfish: number | null;
+  trees: number | null;
+  cars: number | null;
+  perfumes: number | null;
+  [key: string]: number | null;
+}
+
 class AOCDay extends AOC {
-  private store = this.storage.getValueStorage<number>(0);
+  private store = this.storage.getValueStorage<string>('Unknown');
+
+  // Regular Expressions.
+  private parse = /Sue (\d+): (\w+: \d+), (\w+: \d+), (\w+: \d+)/;
+
+  // Fingerprint
+  private fingerprint: AuntSue = {
+    children: 3,
+    cats: 7,
+    samoyeds: 2,
+    pomeranians: 3,
+    akitas: 0,
+    vizslas: 0,
+    goldfish: 5,
+    trees: 3,
+    cars: 2,
+    perfumes: 1,
+  }
 
   override async evaluate(): Promise<void> {
-    for (const v of this.helper.getInput(InputType.SEPARATED_STRING, '')) {
-      if (v === '(') this.store.addNumberToValue(1);
-      else this.store.subtractNumberFromValue(1);
+    for (const v of this.helper.getInput(InputType.LIST, '') as string) {
+      const matches = v.match(this.parse);
+      const id = matches!.slice(1, 2)[0];
+      const sue: Partial<AuntSue> = {}
+
+      for (const match of matches!.slice(2, matches!.length)) {
+        const split = match.split(': ');
+        sue[split[0]!] = parseInt(split[1]!);
+      }
+
+      let same = true;
+      for (const k of Object.keys(sue)) {
+        same = this.fingerprint[k] === sue[k];
+        if (same === false) break;
+      }
+
+      if (same) {
+        this.store.value = `${id}`;
+        break;
+      }
     }
 
     // Store Result of AOC.
--- 2015/16/2.ts
+++ 2015/16/2.ts
@@ -1,22 +1,72 @@
 import { AOC, InputType } from '../../util/state.ts';
 
+interface AuntSue {
+  children: number | null;
+  cats: number | null;
+  samoyeds: number | null;
+  pomeranians: number | null;
+  akitas: number | null;
+  vizslas: number | null;
+  goldfish: number | null;
+  trees: number | null;
+  cars: number | null;
+  perfumes: number | null;
+  [key: string]: number | null;
+}
+
 class AOCDay extends AOC {
-  private store = this.storage.getValueStorage<number>(0);
-  private increment = this.storage.getValueStorage<number>(0);
+  private store = this.storage.getValueStorage<string>('Unknown');
+
+  // Regular Expressions.
+  private parse = /Sue (\d+): (\w+: \d+), (\w+: \d+), (\w+: \d+)/;
+
+  // Fingerprint of Real Sue
+  private fingerprint: AuntSue = {
+    children: 3,
+    cats: 7,
+    samoyeds: 2,
+    pomeranians: 3,
+    akitas: 0,
+    vizslas: 0,
+    goldfish: 5,
+    trees: 3,
+    cars: 2,
+    perfumes: 1,
+  }
 
   override async evaluate(): Promise<void> {
-    for (const v of this.helper.getInput(InputType.SEPARATED_STRING, '')) {
-      if (v === '(') this.store.addNumberToValue(1);
-      else this.store.subtractNumberFromValue(1);
+    for (const v of this.helper.getInput(InputType.LIST, '') as string) {
+      const matches = v.match(this.parse);
+      const id = matches!.slice(1, 2)[0];
+      const sue: Partial<AuntSue> = {}
 
-      this.increment.addNumberToValue(1);
-      if (this.store.value! < 0) {
+      for (const match of matches!.slice(2, matches!.length)) {
+        const split = match.split(': ');
+        sue[split[0]!] = parseInt(split[1]!);
+      }
+
+      let same = true;
+      for (const k of Object.keys(sue)) {
+        if (k === 'cats' || k === 'trees') {
+          same = sue[k]! > this.fingerprint[k]!;
+        }
+        else if (k === 'pomeranians' || k === 'goldfish') {
+          same = sue[k]! < this.fingerprint[k]!;
+        }
+        else {
+          same = sue[k] === this.fingerprint[k]!;
+        }
+        if (same === false) break;
+      }
+
+      if (same) {
+        this.store.value = `${id}`;
         break;
       }
     }
 
     // Store Result of AOC.
-    this.storage.getValueStorage('Unknown', 'value').value = this.increment.getValueAsString();
+    this.storage.getValueStorage('Unknown', 'value').value = this.store.getValueAsString();
   }
 }
 
